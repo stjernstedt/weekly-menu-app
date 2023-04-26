2dc72c2d08da05d548f5d35f2b94529243f2d771
Mattias Stjernstedt <mstjernstedt@gmail.com>
2023-04-26 16:58:08 +0900

    changed layout of landing page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import CreateRecipe from './pages/CreateRecipe';
 import BrowseRecipes from './pages/BrowseRecipes';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import { AppBar, Button, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import BackupTableIcon from '@mui/icons-material/BackupTable';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
 
 const router = createBrowserRouter([
   {
@@ -33,6 +38,40 @@ const App = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <div className="App">
+        <AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <IconButton size='large' color='inherit' sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' sx={{ flexGrow: 1 }}></Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar >
+        <Drawer variant='permanent' sx={{ flexShrink: 0 }}>
+          <Toolbar />
+          <List>
+            {/* does not work */}
+            <ListItemButton LinkComponent={Link} to='/createrecipe'>
+              <ListItemIcon>
+                <AddBoxIcon color='primary' />
+              </ListItemIcon>
+              <ListItemText primary='Add Recipe' />
+            </ListItemButton>
+            <ListItemButton LinkComponent={Link} to='/browserecipes'>
+              <ListItemIcon>
+                <BackupTableIcon color='primary' />
+              </ListItemIcon>
+              <ListItemText primary='Browse Recipes' />
+            </ListItemButton>
+            <ListItemButton LinkComponent={Link} to='/menubuilder'>
+              <ListItemIcon>
+                <NoteAddIcon color='primary' />
+              </ListItemIcon>
+              <ListItemText primary='Create Menu' />
+            </ListItemButton>
+          </List>
+        </Drawer>
+
         <RouterProvider router={router} />
       </div >
     </LocalizationProvider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Stack } from "@mui/material";
+import { Container, Paper, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const buttonStyle = {
@@ -6,22 +6,29 @@ const buttonStyle = {
 	height: '60px',
 }
 
-const Home = () => {
+const Home = (props) => {
 
 	return (
-		<Container>
-			<Stack margin='30vh 0' spacing={2} alignItems='center'>
-				<Link to='createrecipe'>
-					<Button sx={buttonStyle} variant='outlined'>Create Recipe</Button>
-				</Link>
-				<Link to='browserecipes'>
-					<Button sx={buttonStyle} variant='outlined'>Browse Recipes</Button>
-				</Link>
-				<Link to='menubuilder'>
-					<Button sx={buttonStyle} variant='outlined'>Create Menu</Button>
-				</Link>
+		<Container maxWidth='lg' >
+			<Stack marginTop={'20vh'} direction='row' justifyContent='center'>
+				<Paper sx={{ padding: '100px 20px', borderRadius: '100px' }} elevation={5}>
+					<Typography variant='h1'>Weekly Menu Builder</Typography>
+				</Paper>
 			</Stack>
-		</Container>
+		</Container >
+		// <Container>
+		// 	<Stack margin='30vh 0' spacing={2} alignItems='center'>
+		// 		<Link to='createrecipe'>
+		// 			<Button sx={buttonStyle} variant='outlined'>Create Recipe</Button>
+		// 		</Link>
+		// 		<Link to='browserecipes'>
+		// 			<Button sx={buttonStyle} variant='outlined'>Browse Recipes</Button>
+		// 		</Link>
+		// 		<Link to='menubuilder'>
+		// 			<Button sx={buttonStyle} variant='outlined'>Create Menu</Button>
+		// 		</Link>
+		// 	</Stack>
+		// </Container>
 	)
 }
 

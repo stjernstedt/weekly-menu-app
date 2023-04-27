c869324842e202aca7c3d1ab2b5bd36af85f5008
Mattias Stjernstedt <mstjernstedt@gmail.com>
2023-04-28 07:11:59 +0900

    added static.json file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Home from './pages/Home';
 import CreateMenu from './pages/CreateMenu';
 import CreateRecipe from './pages/CreateRecipe';
 import BrowseRecipes from './pages/BrowseRecipes';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { Link, Routes, Route } from 'react-router-dom';
 import { AppBar, Button, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
@@ -13,53 +11,51 @@ import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import CreateIcon from '@mui/icons-material/Create';
 
 const App = () => {
-  document.title = 'Weekly Dish';
+  document.title = 'Weekly Menu';
 
   return (
-    <LocalizationProvider dateAdapter={AdapterMoment}>
-      <div className="App">
-        <AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-          <Toolbar>
-            <IconButton size='large' color='inherit' sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton>
-            <Button LinkComponent={Link} to='/' variant='text' color='inherit'>Weekly Menu</Button>
-            <Typography variant='h6' sx={{ flexGrow: 1 }}></Typography>
-            <Button color='inherit'>Login</Button>
-          </Toolbar>
-        </AppBar >
-        <Drawer variant='permanent' sx={{ flexShrink: 0 }}>
-          <Toolbar />
-          <List>
-            <ListItemButton LinkComponent={Link} to='/createrecipe'>
-              <ListItemIcon>
-                <CreateIcon color='primary' />
-              </ListItemIcon>
-              <ListItemText primary='Add Recipe' />
-            </ListItemButton>
-            <ListItemButton LinkComponent={Link} to='/browserecipes'>
-              <ListItemIcon>
-                <BackupTableIcon color='primary' />
-              </ListItemIcon>
-              <ListItemText primary='Browse Recipes' />
-            </ListItemButton>
-            <ListItemButton LinkComponent={Link} to='/createmenu'>
-              <ListItemIcon>
-                <NoteAddIcon color='primary' />
-              </ListItemIcon>
-              <ListItemText primary='Create Menu' />
-            </ListItemButton>
-          </List>
-        </Drawer>
+    <div className="App">
+      <AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <IconButton size='large' color='inherit' sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Button LinkComponent={Link} to='/' variant='text' color='inherit'>Weekly Menu</Button>
+          <Typography variant='h6' sx={{ flexGrow: 1 }}></Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar >
+      <Drawer variant='permanent' sx={{ flexShrink: 0 }}>
+        <Toolbar />
+        <List>
+          <ListItemButton LinkComponent={Link} to='/createrecipe'>
+            <ListItemIcon>
+              <CreateIcon color='primary' />
+            </ListItemIcon>
+            <ListItemText primary='Add Recipe' />
+          </ListItemButton>
+          <ListItemButton LinkComponent={Link} to='/browserecipes'>
+            <ListItemIcon>
+              <BackupTableIcon color='primary' />
+            </ListItemIcon>
+            <ListItemText primary='Browse Recipes' />
+          </ListItemButton>
+          <ListItemButton LinkComponent={Link} to='/createmenu'>
+            <ListItemIcon>
+              <NoteAddIcon color='primary' />
+            </ListItemIcon>
+            <ListItemText primary='Create Menu' />
+          </ListItemButton>
+        </List>
+      </Drawer>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/createrecipe' element={<CreateRecipe />} />
-          <Route path='/browserecipes' element={<BrowseRecipes />} />
-          <Route path='/createmenu' element={<CreateMenu />} />
-        </Routes>
-      </div >
-    </LocalizationProvider>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/createrecipe' element={<CreateRecipe />} />
+        <Route path='/browserecipes' element={<BrowseRecipes />} />
+        <Route path='/createmenu' element={<CreateMenu />} />
+      </Routes>
+    </div >
   );
 }
 
